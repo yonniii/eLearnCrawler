d79f603a7885b7007380c07814bd68fdd13765cb
yonniii <rkdtjdus753@gmail.com>
2019-08-16 23:51:12 +0900

    push기능 함수로 변경

--- com/crawl/Push.py
+++ com/crawl/Push.py
@@ -41,19 +41,28 @@ class CheckPush:
         # print(self.cursor.fetchall())
         return self.cursor.fetchall()
 
+    def push(self):
+        ids = 'e_yTrAZmLBg:APA91bH_niAX51L10gLi1iXMccpNGjF9XfI34Xws_TnNAsb62r9FaF2iV2IE3eq_ISe4VoH5Irom6pAAIoNP1PWLV4EnnMtesIkBl_2bnqHTqjs5EJHgU897Q-W4gR7LhgmGoj04aFVL'
+        push = Push()
+        list = self.timeDiff('reports',1,'day')
+        for i in list:
+            title = i[1] + ' 마감 1일 전'
+            body = i[1] + ' 마감 1일 전'
+            push.sendFcmNotification(ids, title, body)
+
     def executeQuery(self, sql):
         self.cursor.execute(sql)
         self.db.commit()
 
-ids = 'e_yTrAZmLBg:APA91bH_niAX51L10gLi1iXMccpNGjF9XfI34Xws_TnNAsb62r9FaF2iV2IE3eq_ISe4VoH5Irom6pAAIoNP1PWLV4EnnMtesIkBl_2bnqHTqjs5EJHgU897Q-W4gR7LhgmGoj04aFVL'
-title = 'test'
-body = 'please'
-push = Push()
-# push.sendFcmNotification(ids, title, body)
-
-ch = CheckPush()
-list = ch.timeDiff('reports',1,'day')
-for i in list:
-    title = i[1]+' 마감 1일 전'
-    body = i[1]+' 마감 1일 전'
-    push.sendFcmNotification(ids, title, body)
+# ids = 'e_yTrAZmLBg:APA91bH_niAX51L10gLi1iXMccpNGjF9XfI34Xws_TnNAsb62r9FaF2iV2IE3eq_ISe4VoH5Irom6pAAIoNP1PWLV4EnnMtesIkBl_2bnqHTqjs5EJHgU897Q-W4gR7LhgmGoj04aFVL'
+# title = 'test'
+# body = 'please'
+# push = Push()
+# # push.sendFcmNotification(ids, title, body)
+#
+# ch = CheckPush()
+# list = ch.timeDiff('reports',1,'day')
+# for i in list:
+#     title = i[1]+' 마감 1일 전'
+#     body = i[1]+' 마감 1일 전'
+#     push.sendFcmNotification(ids, title, body)
